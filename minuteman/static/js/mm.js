5b724d620ae4ad86de74844e61bdfd83d1b4b679
David Lance <david6116@yahoo.com>
2012-10-20 10:32:53 -0400

    another update

--- minuteman/static/js/mm.js
+++ minuteman/static/js/mm.js
@@ -10,26 +10,27 @@ $(function(){
         var text_stuff = $('#text').val();
         $.post('/start/', {project:project_id, comments:text_stuff}, function(data){
             $('.start').addClass('started').removeClass('start');
-            $('.tableheader').after('<tr class=hiddenFirst><td>' + project_id + '</td><td>'+ project_id +'</td><td>0</td><td>' + text_stuff + '</td><td id=topRight></td></tr>');
+            $('.tableheader').after('<tr><td>' + project_id + '</td><td>'+ project_id +'</td><td>0</td><td>' + text_stuff + '</td><td id=topRight></td></tr>');
             $('#newproject').toggle();
             $('.oneliner').toggle();
             $('.stop').appendTo( $('#topRight') );
             $('input[name="project"]').val(project_id);
-            $('#finish').toggle();
+            $('#finish').val(project_id).toggle();
+            $('.rows').last().remove();
 
         });
         return false;
     });
 
     $('.stop').click(function(e){
-        var project_id = $('#id_project').val();
+        var project_id = $('#finish').val();
         $.post('/stop/', {project:project_id}, function(data){
-
+            $('.continue').first().toggle();
             $('#finish').toggle();
-            $('.hiddenFirst').toggle();
             $('#newproject').toggle();
             $('.oneliner').toggle();
-
+            $('#id_project').val('');
+            $('#text').val('');
         });
         return false
     });
